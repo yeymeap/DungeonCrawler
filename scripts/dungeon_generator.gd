4b940be51f1b9f6674bcba234bb5d067c0de8ebe
Bence Balázs Botló <bence3728@gmail.com>
2025-10-14 23:59:44 +0200

    Add room padding to dungeon generation
    Rooms now include a padding area to prevent overlap, tracked in a new padded_rooms array. The background tile for padding is configurable, and padding tiles are placed around each room during generation, for debugging purposes.

--- scripts/dungeon_generator.gd
+++ scripts/dungeon_generator.gd
@@ -16,8 +16,10 @@ extends Node2D
 @export var bg_tile_id: Vector2i = Vector2i(0, 14)
 @export var floor_tile_id: Vector2i = Vector2i(0, 12)
 @export var wall_tile_id: Vector2i = Vector2i(7, 0)
+@export var padding_tile_id: Vector2i = Vector2i(1, 14)
 
 var rooms: Array[Rect2i] = []
+var padded_rooms: Array[Rect2i] = []
 var room_center: Array[Vector2i] = []
 
 func _ready():
@@ -36,19 +38,27 @@ func fill_background(): # fill with background tiles
 	for y in range(-100, 100):
 		for x in range(-100, 100):
 			background_tilemaplayer.set_cell(Vector2i(x, y), 0, bg_tile_id)
+			
+func get_room_center(room: Rect2i) -> Vector2i:
+	return room.position + room.size / 2
 
 func generate_room(origin: Vector2i): # generate rooms
 	var room_height = randi_range(room_height_min, room_height_max)
 	var room_width = randi_range(room_width_min, room_width_max)
+	var room_padding = 2
 	
-	var new_room = Rect2i(origin, Vector2(room_width, room_height)) # TODO make padding
-
-	for room in rooms:
-		if new_room.intersects(room):
+	var padded_room = Rect2i(
+		origin - Vector2i(room_padding, room_padding),
+		Vector2i(room_width + room_padding * 2, room_height + room_padding * 2)
+		)
+		
+	for room in padded_rooms:
+		if padded_room.intersects(room):
 			return false
-	
+			
+	padded_rooms.append(padded_room)
+	var new_room = Rect2i(origin, Vector2i(room_width, room_height)) # TODO make padding
 	rooms.append(new_room)
-	room_center.append(origin + Vector2i(room_width / 2, room_height / 2))
 	
 	for y in range(room_height):
 		for x in range(room_width):
@@ -57,5 +67,12 @@ func generate_room(origin: Vector2i): # generate rooms
 				wall_tilemaplayer.set_cell(pos, 0, wall_tile_id)
 			else:
 				floor_tilemaplayer.set_cell(pos, 0, floor_tile_id)
+	
+	for y in range(padded_room.size.y):
+		for x in range(padded_room.size.x):
+			var pos = padded_room.position + Vector2i(x, y)
+			if not new_room.has_point(pos):
+				background_tilemaplayer.set_cell(pos, 0, padding_tile_id)
+				
 	return true
 	
